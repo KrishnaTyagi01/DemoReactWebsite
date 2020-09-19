a52d00a0bff7eda113a59e975f9a24bc773466cb
krishna <tyagikrishna38@gmail.com>
2020-09-19 16:53:01 +0530

    Updates

---
+++ precache-manifest.5022910dd335a0f971537497b5a080c4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362341b73a8f8c27d5290d4bc0b82b17",
+    "url": "/DemoReactWebsite/index.html"
+  },
+  {
+    "revision": "8b23de1c6b1db6caede1",
+    "url": "/DemoReactWebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b33bea49aff9c5906b99",
+    "url": "/DemoReactWebsite/static/css/main.0d07fa04.chunk.css"
+  },
+  {
+    "revision": "8b23de1c6b1db6caede1",
+    "url": "/DemoReactWebsite/static/js/2.fef13138.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/DemoReactWebsite/static/js/2.fef13138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33bea49aff9c5906b99",
+    "url": "/DemoReactWebsite/static/js/main.61551d9f.chunk.js"
+  },
+  {
+    "revision": "9ce99d75846d4e63e8ae",
+    "url": "/DemoReactWebsite/static/js/runtime-main.d61e9769.js"
+  },
+  {
+    "revision": "2001b5f4fcd7583553d14b08d5ab9b96",
+    "url": "/DemoReactWebsite/static/media/android.2001b5f4.jpg"
+  },
+  {
+    "revision": "71f2eb0762a88e0ddb15a9fa5edd3666",
+    "url": "/DemoReactWebsite/static/media/app.71f2eb07.jpg"
+  },
+  {
+    "revision": "8d00948f7651baa4466bef28a1975d40",
+    "url": "/DemoReactWebsite/static/media/digital.8d00948f.jpg"
+  },
+  {
+    "revision": "8e060913e2f3d9adcedaf2cfc22f464b",
+    "url": "/DemoReactWebsite/static/media/fin.8e060913.jpg"
+  },
+  {
+    "revision": "ae5c78373d88f50cd1af4159dfd7c887",
+    "url": "/DemoReactWebsite/static/media/interview.ae5c7837.svg"
+  },
+  {
+    "revision": "90aba45f585061bddb7d72c2f23da98f",
+    "url": "/DemoReactWebsite/static/media/soft.90aba45f.jpg"
+  },
+  {
+    "revision": "65a18ad38acb5dc5f1373508687b0253",
+    "url": "/DemoReactWebsite/static/media/web.65a18ad3.jpg"
+  }
+]);
